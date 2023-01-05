9804c34315dc5fd9987837e92d234b46df12ba3b
bohjaangles <bohjaangles@gmail.com>
2023-01-04 23:01:08 -0600

    add 3 button components and logic for picking an image from local lib

--- App.js
+++ App.js
@@ -1,16 +1,46 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { StyleSheet, View } from 'react-native';
+import Button from './components/Button';
 import ImageViewer from './components/ImageViewer';
+import * as ImagePicker from 'expo-image-picker';
 
-const PlaceHolderImage = require('./assets/images/background-image.png');
+const PlaceholderImage = require('./assets/images/background-image.png');
 
 export default function App() {
+  const [selectedImage, setSelectedImage] = useState(null);
+  const [showAppOptions, setShowAppOptions] = useState(false)
+
+  const pickImageAsync = async () => {
+    let result = await ImagePicker.launchImageLibraryAsync({
+      allowsEditing: true,
+      quality: 1,
+    });
+
+    if (!result.canceled) {
+      setSelectedImage(result.assets[0].uri);
+      setShowAppOptions(true);
+    } else {
+      alert('You did not select any image.');
+    }
+  };
+
   return (
     <View style={styles.container}>
       <View style={styles.imageContainer}>
-        <ImageViewer placeholderImageSource={PlaceHolderImage} />
+        <ImageViewer 
+          placeholderImageSource={PlaceholderImage}
+          selectedImage={selectedImage} 
+        />
       </View>
-      <Text style={{color: '#fff'}}>Hello World!</Text>
+      {showAppOptions ? (
+        <View />
+      ) : (  
+        <View style={styles.footerContainer}>
+          <Button theme="primary" label="Choose a photo" onPress={pickImageAsync} />
+          <Button label="Use this photo" onPress={() => setShowAppOptions(true)} />
+        </View>
+      )}
       <StatusBar style="auto" />
     </View>
   );
@@ -24,12 +54,17 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
   imageContainer: {
-    felx: 1,
+    flex: 1,
     paddingTop: 58,
   },
   image: {
     width: 320,
     height: 440,
     borderRadius: 18,
-  }
+  },
+  footerContainer: {
+    flex: 1 / 3,
+    alignItems: 'center',
+  },
 });
+
